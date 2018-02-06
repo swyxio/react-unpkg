8ddf2a4b0774d7961c39ff8cf087ae9b7fc83f05
sw-yx <swyx@swyx.io>
2018-02-06 00:30:43 -0500

    rebuilt without addons - just the saved packages

---
+++ browser/Plain.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Plain extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is just a plain page</p>
+        <p>why not check out the About page?</p>
+      </div>
+    );
+  }
+}
+
+export default Plain;
--- browser/index.js
+++ browser/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./Main";
+import Plain from "./Plain";
 import About from "./About";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -9,7 +9,7 @@ const BasicExample = () => (
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Plain</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
@@ -18,7 +18,7 @@ const BasicExample = () => (
 
       <hr />
 
-      <Route exact path="/" component={Main} />
+      <Route exact path="/" component={Plain} />
       <Route path="/about" component={About} />
     </div>
   </Router>
